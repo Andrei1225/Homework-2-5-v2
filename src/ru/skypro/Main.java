3dfad33b47ab4d9866ffc780c460eea4ab8717ae
Андрей <andybro17@yandex.ru>
2022-08-23 14:29:55 +0300

    Поправил код.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -5,63 +5,61 @@ import ru.skypro.exceptions.WrongPasswordException;
 
 public class Main {
     public static void main(String[] args) {
-        loginCheck(login);
         loginExceptionsCheck();
-        passwordCheck(password, confirmPassword);
         passwordExceptionsCheck();
     }
     public static String login = "java_skypr2";
     public static String password = "D_1hWiKjjP_9";
-    public static String confirmPassword = "D_1hWiKjjP_90";
+    public static String confirmPassword = "D_1hWiKjjP_9";
 
     public static boolean loginCheck(String login) {
-        if (login.matches("^\\w{1,20}$")) {
-            System.out.println("Логин соответствует требованиям");
-            return true;
+        if (login.matches("^\\w{1,}$")) {
+            if (login.length() <= 20) {
+                System.out.println("Логин соответствует требованиям");
+                return true;
+            } else {
+                throw new WrongLoginException();
+            }
         } else {
             System.out.println("Логин не соответствует требованиям");
-            return false;
-        }
-    }
-
-    public static void loginLengthCheck(String login) {
-        if (login.length() > 20) {
             throw new WrongLoginException();
         }
     }
 
-
-    public static void passwordEqualsCheck(String password, String confirmPassword) {
+    public static boolean passwordCheck(String password, String confirmPassword) {
         if (password.equals(confirmPassword)) {
+            if (password.matches("^\\w{1,}$")) {
+                if (password.length() < 20) {
+                    System.out.println("Пароль соответствует требованиям");
+                    return true;
+                } else {
+                    System.out.println("Пароль слишком длинный!");
+                    throw new WrongPasswordException();
+                }
+            } else {
+                System.out.println("Пароль не соответствует требованиям");
+                throw new WrongPasswordException();
+            }
         } else {
+            System.out.println("Пароли не совпадают");
             throw new WrongPasswordException();
         }
     }
 
-
-    public static boolean passwordCheck(String password, String confirmPassword) {
-        if (password.equals(confirmPassword) && password.matches("^\\w{1,19}$")) {
-            System.out.println("Пароль соответствует требованиям");
-            return true;
-        } else {
+    private static boolean loginExceptionsCheck() {
+        try {
+            loginCheck(login);
+        } catch (WrongLoginException e) {
             return false;
         }
+        return false;
     }
-
-    private static void loginExceptionsCheck() {
+    private static boolean passwordExceptionsCheck() {
         try {
-            loginLengthCheck(login);
-        } catch (WrongLoginException e) {
-            System.out.println("Длина логина не может быть больше 20 символов");
-        }
-
-    }
-    private static void passwordExceptionsCheck() {
-        try {
-            passwordEqualsCheck(password,confirmPassword);
+            passwordCheck(password, confirmPassword);
         } catch (WrongPasswordException e) {
-            System.out.println("Пароли не совпадают");
+            return  false;
         }
-
+        return false;
     }
 }
